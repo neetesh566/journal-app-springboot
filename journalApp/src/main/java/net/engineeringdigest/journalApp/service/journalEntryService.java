5913241df588d5632476a2551ffcac0c99855c53
neetesh566 <shakyaneetesh2@gmail.com>
2026-03-12 18:14:17 +0530

    use the @Transcational and setup the mongodb atlas

--- journalApp/src/main/java/net/engineeringdigest/journalApp/service/journalEntryService.java
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/service/journalEntryService.java
@@ -7,6 +7,7 @@ import net.engineeringdigest.journalApp.repository.journalEntryRepository;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -21,17 +22,18 @@ public class journalEntryService {
     @Autowired
     private UserService userService;
 
+    @Transactional
     public void saveEntry(JournalEntry journalEntry, String  userName){
-        try{
-            User user = userService.findByUserName(userName);
-            journalEntry.setDate(LocalDateTime.now());
-            JournalEntry saved= journalEntryRepository.save(journalEntry);
-            user.getJournalEntries().add(saved);
-            userService.saveEntry(user);
-
-        } catch (Exception ex){
-            log.error("Exception",ex);
-        }
+            try{
+                User user = userService.findByUserName(userName);
+                journalEntry.setDate(LocalDateTime.now());
+                JournalEntry saved= journalEntryRepository.save(journalEntry);
+                user.getJournalEntries().add(saved);
+                userService.saveEntry(user);
+            } catch (Exception e){
+                System.out.println(e);
+                throw new RuntimeException("An error occured while saving journal entry",e);
+            }
 
     }
 
